249558b2b24fb15c6c3f2d57ef1827cd0d8a9b6f
ahmedzein <elze0010@algonquinlive.com>
2015-02-11 17:21:27 -0500

    output text in js
    h1 dl dt dd, for all the berries

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 var blueberries = {
-    firtName:'Vaccinium corymbosum',
+    firstName:'Vaccinium corymbosum',
     growsOnShrub: true,
     energy: 240,
     carbohydrates: 14.49,
     protein: 0.74
 };
 
-console.log(blueberries.firtName);
-
 var grapes = {
-    firtName:'Vitis vinifera',
+    firstName:'Vitis vinifera',
     growsOnShrub: false,
     energy: 288,
     carbohydrates: 148.1,
@@ -17,9 +15,35 @@ var grapes = {
 };
 
 var redCurrant = {
-    firtName:'Ribes Rubrum',
+    firstName:'Ribes Rubrum',
     growsOnShrub: true,
     energy: 234,
     carbohydrates: 13.8,
     protein: 1.4
 };
+
+var writeBerryInfo = function (berry) {
+    document.write('<h1>' + berry.firstName + '</h1>')
+    
+    document.write ('<dl>');
+    
+    document.write('<dt>Grows On Shrub</dt>');
+    document.write('<dd>' + berry.growsOnShrub + '</dd>');
+    
+    document.write('<dt>Energy</dt>');
+    document.write('<dd>' + berry.energy + '</dd>');
+    
+    document.write('<dt>Carbohydrates</dt>');
+    document.write('<dd>' + berry.carbohydrates + '</dd>');
+    
+    document.write('<dt>Protein</dt>');
+    document.write('<dd>' + berry.protein + '</dd>');
+        
+    document.write ('<dl>');
+    
+    
+};
+
+writeBerryInfo(blueberries);
+writeBerryInfo(grapes);
+writeBerryInfo(redCurrant);
