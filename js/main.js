580cf39861a64ef17e20bde4e9532fb0e1de5dd9
ahmedzein <elze0010@algonquinlive.com>
2015-02-11 16:53:49 -0500

    added all three pages
    html css js
    
    added code to js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var blueberries = {
+    firtName:'Vaccinium corymbosum',
+    growsOnShrub: true,
+    energy: 240,
+    carbohydrates: 14.49,
+    protein: 0.74
+};
+
+console.log(blueberries.firtName);
+
+var grapes = {
+    firtName:'Vitis vinifera',
+    growsOnShrub: false,
+    energy: 288,
+    carbohydrates: 148.1,
+    protein: 0.72
+};
+
+var redCurrant = {
+    firtName:'Ribes Rubrum',
+    growsOnShrub: true,
+    energy: 234,
+    carbohydrates: 13.8,
+    protein: 1.4
+};
